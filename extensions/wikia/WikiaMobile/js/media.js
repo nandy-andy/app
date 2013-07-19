871bfc4675614850b13a4ddb0547a0836efe084f
lizlux <liz@lizlux.net>
2013-07-19 12:23:12 +0200

    MOB-238 feedback from code review

--- extensions/wikia/WikiaMobile/js/media.js
+++ extensions/wikia/WikiaMobile/js/media.js
@@ -177,7 +177,7 @@ define('media', ['JSMessages', 'modal', 'throbber', 'wikia.querystring', require
 
 	function setupImage(){
 		var video,
-			imgTitle,
+			imgTitle = currentMedia.name,
 			// cache value for clickSource to prevent race conditions
 			cs = clickSource;
 
@@ -191,8 +191,6 @@ define('media', ['JSMessages', 'modal', 'throbber', 'wikia.querystring', require
 		}
 
 		if(currentMedia.type == Media.types.VIDEO) {
-			imgTitle = currentMedia.name;
-
 			zoomable = false;
 
 			if(videoCache[imgTitle]){
@@ -247,12 +245,10 @@ define('media', ['JSMessages', 'modal', 'throbber', 'wikia.querystring', require
 				}
 			}
 			// update url for sharing
-			qs().setVal('file', imgTitle, true ).replaceState();
+			qs().setVal( 'file', imgTitle, true ).replaceState();
 		}else if(currentMedia.type == Media.types.IMAGE){
 			var img = new Image();
 			img.src = currentMedia.url;
-
-			imgTitle = currentMedia.name;
 
 			zoomable = true;
 
@@ -286,7 +282,7 @@ define('media', ['JSMessages', 'modal', 'throbber', 'wikia.querystring', require
 				origH = img.height;
 			}
 			// update url for sharing
-			qs().setVal('file', imgTitle, true ).replaceState();
+			qs().setVal( 'file', imgTitle, true ).replaceState();
 		} else if(currentMedia.type){//custom
 			var data = {
 					currentNum: currentNum,
